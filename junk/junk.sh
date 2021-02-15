0e0e9879753a90d80dd2c6f8108d13344dffcfb2
jgerega107 <jakegerega107@gmail.com>
2021-02-15 17:59:21 -0500

    Finalized junk script

--- junk/junk.sh
+++ junk/junk.sh
@@ -4,6 +4,20 @@ help_flag=0
 junk_flag=0
 purge_flag=0
 
+readonly junk="$HOME/.junk/"
+base=$(basename "$0")
+
+if [ $# -eq 0 ]; then
+  cat <<HERE
+Usage: $base [-hlp] [list of files]
+   -h: Display help.
+   -l: List junked files.
+   -p: Purge all files.
+   [list of files] with no other arguments to junk those files.
+HERE
+  exit 
+fi
+
 while getopts ":hlp" option; do
     case "$option" in
         h) help_flag=1
@@ -12,37 +26,47 @@ while getopts ":hlp" option; do
         ;;
         p) purge_flag=1
         ;;
-        ?) printf "Error: Unknown option '-%s'.\n" $OPTARG       
-        exit 1
+        ?) 
+        help_flag=1  
+        printf "Error: Unknown option '-%s'.\n" $OPTARG 
         ;;
     esac
 done
 
-junk="$HOME/.junk/"
 if [ ! -d "$junk" ]; then
-    mkdir $junk
+    mkdir "$junk"
+fi
+
+if [ $junk_flag -eq 0 ] && [ $help_flag -eq 0 ] && [ $purge_flag -eq 0 ]; then
+    shift "$((OPTIND-1))"
+    for f in "$@"; do
+        if [ -f "$f" ]; then
+            mv $f $junk
+        elif [ -d "$f" ]; then
+            mv $f $junk
+        else
+            echo "Warning: '$f' not found."
+        fi
+    done
 fi
 
 if [ $help_flag -eq 1 ]; then
-    #do help document
-    echo "Help"
+    cat <<HERE
+Usage: $base [-hlp] [list of files]
+   -h: Display help.
+   -l: List junked files.
+   -p: Purge all files.
+   [list of files] with no other arguments to junk those files.
+HERE
 fi
 
 if [ $junk_flag -eq 1 ]; then
-    echo "Junk"
+    ls -Al "$junk"
 fi
 
 if [ $purge_flag -eq 1 ]; then
-    echo "Purge"
+    rm -r "$junk"
+    mkdir "$junk"
 fi
 
-shift "$((OPTIND-1))"
-index=0
-for f in "$@"; do
-    if [ -f "$f" ]; then
-        mv $f $junk
-    else
-        echo "Warning: '$f' not found."
-    fi
-done
 
