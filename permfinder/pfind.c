51514306b6522610f712175edab9f3faae00a1c7
jgerega107 <jakegerega107@gmail.com>
2021-03-16 19:58:06 -0400

    Finalized permfinder, cleaned up dslab, added tests

--- permfinder/pfind.c
+++ permfinder/pfind.c
@@ -7,53 +7,88 @@
 #include <getopt.h>
 #include <sys/stat.h>
 
+//Author: Jacob Gerega
+//Pledge: I pledge my honor that I have abided by the Stevens Honor System.
+
 int perms[] = {S_IRUSR, S_IWUSR, S_IXUSR,
                S_IRGRP, S_IWGRP, S_IXGRP,
                S_IROTH, S_IWOTH, S_IXOTH};
 
-void rdir(char* p){
+char *pms;
+
+void rdir(char *p)
+{
     DIR *dir;
-    if((dir = opendir(p)) == NULL){
-        fprintf(stderr, "Cannot open dir");
+    if ((dir = opendir(p)) == NULL)
+    {
+        fprintf(stderr, "Error: Cannot open directory '%s'. %s.\n", p, strerror(errno));
         return;
     }
-
     struct dirent *entry;
     struct stat buf;
-    char filename[PATH_MAX+1];
+    char filename[PATH_MAX + 1];
     size_t pathlen = 0;
 
     filename[0] = '\0';
-    if(strcmp(p, "/")){
+    if (strcmp(p, "/"))
+    {
         strncpy(filename, p, PATH_MAX);
     }
     pathlen = strlen(filename) + 1;
     filename[pathlen - 1] = '/';
     filename[pathlen] = '\0';
-    
-    while((entry = readdir(dir)) != NULL){
-        if(strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0){
+
+    while ((entry = readdir(dir)) != NULL)
+    {
+        if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0)
+        {
             continue;
         }
         strncpy(filename + pathlen, entry->d_name, PATH_MAX - pathlen);
-        if(lstat(filename, &buf) < 0){
-            fprintf(stderr, "Error: Cannot stat file");
+        if (lstat(filename, &buf) < 0)
+        {
+            fprintf(stderr, "Error: Cannot stat file\n");
             continue;
         }
-        if(entry->d_type == DT_DIR){
-            rdir(filename);
-        }
-        else{
-            int matching = 1;
-            for(int i = 0; i < 9; i+=3){
-                if(!(buf.st_mode & perms[i]) && 
-                !(buf.st_mode & perms[i+1]) &&
-                !(buf.st_mode & perms[i+2])){
-                    matching = 0;
+        else
+        {
+            char *fpms = malloc(10);
+            fpms[9] = '\0';
+            for (int i = 0; i < 9; i += 3)
+            {
+                if (buf.st_mode & perms[i])
+                {
+                    fpms[i] = 'r';
+                }
+                else
+                {
+                    fpms[i] = '-';
+                }
+                if (buf.st_mode & perms[i + 1])
+                {
+                    fpms[i + 1] = 'w';
+                }
+                else
+                {
+                    fpms[i + 1] = '-';
+                }
+                if (buf.st_mode & perms[i + 2])
+                {
+                    fpms[i + 2] = 'x';
+                }
+                else
+                {
+                    fpms[i + 2] = '-';
                 }
             }
-            if(matching == 1){
+            if (strcmp(pms, fpms) == 0)
+            {
                 printf("%s\n", filename);
+            }
+            free(fpms);
+            if (entry->d_type == DT_DIR)
+            {
+                rdir(filename);
             }
         }
     }
@@ -61,55 +96,88 @@ void rdir(char* p){
     closedir(dir);
 }
 
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[])
+{
     char path[PATH_MAX];
-    char* pms;
+    char fold[PATH_MAX];
     int dir = 0;
+    int nodiropts = 0;
     int perm = 0;
+    int nopermopts = 0;
     int opt;
-    while((opt = getopt(argc, argv, "d:p:")) != -1){
-        switch(opt){
-            case 'd':
-                dir = 1;
-                if(realpath(optarg, path) == NULL){
-                    fprintf(stderr, "Error: Cannot stat '%s'. %s.\n", optarg, strerror(errno));
-                    return EXIT_FAILURE;
-                }
-                break;
-            case 'p':
-                perm = 1;
-                //first check if the perm string is valid
-                if(strlen(optarg) == 9){
-                    for(int i = 0; i < 9; i+=3){
-                        if(!((optarg[i] == 'r' || optarg[i] == '-') &&
-                        (optarg[i+1] == 'w' || optarg[i+1] == '-') &&
-                        (optarg[i+2] == 'x' || optarg[i+2] == '-'))){
-                            fprintf(stderr, "Error: Permissions string '%s' is invalid.\n", optarg);
-                            return EXIT_FAILURE;
-                        }
+    while ((opt = getopt(argc, argv, ":d:p:h")) != -1)
+    {
+        switch (opt)
+        {
+        case 'h':
+            printf("Usage: %s -d <directory> -p <permissions string> [-h]\n", argv[0]);
+            return EXIT_SUCCESS;
+        case 'd':
+            dir = 1;
+            strcpy(fold, optarg);
+            break;
+        case 'p':
+            perm = 1;
+            //check perms string
+            if (strlen(optarg) == 9)
+            {
+                for (int i = 0; i < 9; i += 3)
+                {
+                    if (!((optarg[i] == 'r' || optarg[i] == '-') &&
+                          (optarg[i + 1] == 'w' || optarg[i + 1] == '-') &&
+                          (optarg[i + 2] == 'x' || optarg[i + 2] == '-')))
+                    {
+                        fprintf(stderr, "Error: Permissions string '%s' is invalid.\n", optarg);
+                        return EXIT_FAILURE;
                     }
                 }
-                else{
-                    fprintf(stderr, "Error: Permissions string '%s' is invalid.\n", optarg);
-                    return EXIT_FAILURE;
-                }
-                for(int i = 0; i < 9; i+=3){
-                    if(optarg[i] != 'r'){
-                        perms[i] = ~perms[i];
-                    }
-                    if(optarg[i+1] != 'w'){
-                        perms[i+1] = ~perms[i+1];
-                    }
-                    if(optarg[i+2] != 'x'){
-                        perms[i+2] = ~perms[i+2];
-                    }
-                }
-                break;
-            case '?':
-                printf("Error: Unknown option '-%c' received.\n", optopt);
+            }
+            else
+            {
+                fprintf(stderr, "Error: Permissions string '%s' is invalid.\n", optarg);
                 return EXIT_FAILURE;
+            }
+            pms = malloc(10);
+            strcpy(pms, optarg);
+            break;
+        case ':':
+            if (optopt == 'd')
+            {
+                nodiropts = 1;
+            }
+            else if (optopt == 'p')
+            {
+                nopermopts = 1;
+            }
+            break;
+        case '?':
+            printf("Error: Unknown option '-%c' received.\n", optopt);
+            return EXIT_FAILURE;
         }
     }
+
+    if (dir == 0 && perm == 0 && nodiropts == 0 && nopermopts == 0)
+    {
+        printf("Usage: %s -d <directory> -p <permissions string> [-h]\n", argv[0]);
+        return EXIT_SUCCESS;
+    }
+    if (dir == 0 || nodiropts == 1)
+    {
+        printf("Error: Required argument -d <directory> not found.\n");
+        return EXIT_FAILURE;
+    }
+    if (perm == 0 || nopermopts == 1)
+    {
+        printf("Error: Required argument -p <permissions string> not found.\n");
+        return EXIT_FAILURE;
+    }
+
+    if (realpath(fold, path) == NULL)
+    {
+        fprintf(stderr, "Error: Cannot stat '%s'. %s.\n", fold, strerror(errno));
+        return EXIT_FAILURE;
+    }
     rdir(path);
+    free(pms);
     return EXIT_SUCCESS;
 }
