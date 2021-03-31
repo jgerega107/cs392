46d6ea6b2bd4e23e2eb2e6efc3641ceac988101e
jgerega107 <jakegerega107@gmail.com>
2021-03-30 20:03:45 -0400

    Added grepprocess and pipelab

---
+++ sortedpfind/spfind.c
@@ -0,0 +1,88 @@
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[])
+{
+
+    int pfind_to_sort[2], sort_to_parent[2];
+    if (pipe(pfind_to_sort) < 0)
+    {
+        fprintf(stderr, "Error: Cannot create pipe pfind_to_sort. %s.\n", strerror(errno));
+        return EXIT_FAILURE;
+    }
+    if (pipe(sort_to_parent) < 0)
+    {
+        fprintf(stderr, "Error: Cannot create pipe sort_to_parent. %s.\n", strerror(errno));
+        return EXIT_FAILURE;
+    }
+
+    pid_t pid[2];
+    if ((pid[0] = fork()) == 0)
+    {
+        close(pfind_to_sort[0]);
+        dup2(pfind_to_sort[1], STDOUT_FILENO);
+
+        close(sort_to_parent[0]);
+        close(sort_to_parent[1]);
+
+        if (execl("pfind", "pfind", "-d", argv[2], "-p", argv[4], NULL) < 0)
+        {
+            fprintf(stderr, "Error: pfind failed. %s.\n", strerror(errno));
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    if ((pid[1] = fork()) == 0)
+    {
+        close(pfind_to_sort[1]);
+        dup2(pfind_to_sort[0], STDIN_FILENO);
+        close(sort_to_parent[0]);
+        dup2(sort_to_parent[1], STDOUT_FILENO);
+
+        if (execlp("sort", "sort", NULL) < 0)
+        {
+            fprintf(stderr, "Error: sort failed. %s.\n", strerror(errno));
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    close(sort_to_parent[1]);
+    dup2(sort_to_parent[0], STDIN_FILENO);
+    close(pfind_to_sort[0]);
+    close(pfind_to_sort[1]);
+
+    char buffer[4096];
+    while (1)
+    {
+        ssize_t count = read(STDIN_FILENO, buffer, sizeof(buffer));
+        if (count == -1)
+        {
+            if (errno == EINTR)
+            {
+                continue;
+            }
+            else
+            {
+                perror("read()");
+                exit(EXIT_FAILURE);
+            }
+        }
+        else if (count == 0)
+        {
+            break;
+        }
+        else
+        {
+            write(STDOUT_FILENO, buffer, count);
+        }
+    }
+    close(sort_to_parent[0]);
+    wait(NULL);
+    wait(NULL);
+
+    return EXIT_SUCCESS;
+}
