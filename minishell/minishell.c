ac4574f903cdac4233c8c8f325f692a08144cd49
jgerega107 <jakegerega107@gmail.com>
2021-04-12 19:37:40 -0400

    Updated minishell

--- minishell/minishell.c
+++ minishell/minishell.c
@@ -6,6 +6,8 @@
 #include <unistd.h>
 #include <pwd.h>
 #include <errno.h>
+#include <sys/wait.h>
+#include <limits.h>
 
 
 #define BRIGHTBLUE "\x1b[34;1m"
@@ -13,38 +15,66 @@
 
 int main(int argc, char* argv[]){
     while(true){
-        size_t buflen = 64;
-        char cwd[buflen];
-        printf("\n[%s%s%s]$ ", BRIGHTBLUE, getcwd(cwd, buflen), DEFAULT);
-        char* line[buflen];
-        getline(line, &buflen, stdin);
+        char cwd[PATH_MAX]; //current working directory
+        printf("[%s%s%s]$ ", BRIGHTBLUE, getcwd(cwd, PATH_MAX), DEFAULT); //print curr directory
+        char line[PATH_MAX]; //line input
+        fgets(line, PATH_MAX, stdin); //read in line input
+        line[strlen(line)-1] = '\0';
 
-        if(strcmp(*line, "exit\n") == 0){
+        char* token = strtok(line, " \t");
+        char* tokens[PATH_MAX];
+        int tc = 0;
+        tokens[0] = token;
+        while(token != NULL){
+            tokens[tc] = token;
+            tc++;
+            token = strtok(NULL, " \t");
+        }
+
+        if(strcmp(tokens[0], "exit") == 0){ //exit program
             break;
         }
-        else if(strstr(*line, "cd")){
-            char* dir = strchr(*line, ' ');
-            //goto directory given
-            if(dir){
-                dir++;
-                dir[strlen(dir)-1] = '\0';
-                if(chdir(dir) != 0){
+        else if(strcmp(tokens[0], "cd") == 0){
+            if(tc > 1){
+                if(strcmp(tokens[1], "~") == 0){
+                    uid_t uid = getuid();
+                    struct passwd *pw = getpwuid(uid);
+                    if(chdir(pw->pw_dir) != 0){
+                        fprintf(stderr, "Error: %s.\n", strerror(errno));
+                    }
+                }
+                else if(chdir(tokens[1]) != 0){
                     fprintf(stderr, "Error: %s.\n", strerror(errno));
                 }   
             }
-            //else go home
-            else if(!dir || strcmp(dir, "~") == 0){
+            else{
                 uid_t uid = getuid();
                 struct passwd *pw = getpwuid(uid);
                 if(chdir(pw->pw_dir) != 0){
                     fprintf(stderr, "Error: %s.\n", strerror(errno));
                 }
             }
-            printf("%ld\n", strlen(dir));
-
         }
-        else{
-
+        else if(tokens[0] != NULL){
+            pid_t pid;
+            if((pid = fork()) == 0){
+                if(tc > 1){
+                    if(execvp(tokens[0], tokens) < 0){
+                        fprintf(stderr, "Error: exec() failed. %s.\n", strerror(errno));
+                        exit(EXIT_FAILURE);
+                    }
+                }
+                else{
+                    if(execlp(tokens[0], tokens[0], NULL) < 0){
+                        fprintf(stderr, "Error: exec() failed. %s.\n", strerror(errno));
+                        exit(EXIT_FAILURE);
+                    }
+                }
+                
+            }
+            if(waitpid(pid, NULL, 0) < 0){
+                fprintf(stderr, "Error: wait() failed. %s.\n", strerror(errno));
+            }
         }
     }
     return EXIT_SUCCESS;
