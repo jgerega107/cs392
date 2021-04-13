50c48c87c15f3cf5414677624143f1dfadca566b
jgerega107 <jakegerega107@gmail.com>
2021-04-13 15:13:37 -0400

    Finalized minishell

--- minishell/minishell.c
+++ minishell/minishell.c
@@ -13,6 +13,17 @@
 #define BRIGHTBLUE "\x1b[34;1m"
 #define DEFAULT    "\x1b[0m"
 
+/**
+ * Author: Jacob Gerega
+ * Pledge: I pledge my honor that I have abided by the Stevens Honor System.
+ ***/
+
+volatile sig_atomic_t signal_value = 0;
+
+void catch_signal(int sig){
+    signal_value = sig;
+}
+
 int main(int argc, char* argv[]){
     while(true){
         char cwd[PATH_MAX]; //current working directory
@@ -53,11 +64,19 @@ int main(int argc, char* argv[]){
                 uid_t uid = getuid();
                 struct passwd *pw = getpwuid(uid);
                 if(chdir(pw->pw_dir) != 0){
-                    fprintf(stderr, "Error: %s.\n", strerror(errno));
+                    fprintf(stderr, "Error: %s.\n", strerror(errno)) ;
                 }
             }
         }
         else if(tokens[0] != NULL){
+            struct sigaction action;
+            memset(&action, 0, sizeof(struct sigaction));
+            action.sa_handler = catch_signal;
+            action.sa_flags = SA_RESTART;
+            if(sigaction(SIGINT, &action, NULL) == -1){
+                perror("sigaction");
+                return EXIT_FAILURE;
+            }
             pid_t pid;
             if((pid = fork()) == 0){
                 if(tc > 1){
@@ -73,6 +92,13 @@ int main(int argc, char* argv[]){
                     }
                 }
                 
+            }
+            if(signal_value == SIGINT){
+                if(kill(pid, SIGINT) == -1){
+                    fprintf(stderr, "Error: kill() failed. %s.\n", strerror(errno));
+                    return EXIT_FAILURE;
+                }
+                signal_value = 0;
             }
             if(waitpid(pid, NULL, 0) < 0){
                 fprintf(stderr, "Error: wait() failed. %s.\n", strerror(errno));
