c681263ec8ae1a3d5a1155211194500c977d37a1
jgerega107 <jakegerega107@gmail.com>
2021-05-04 21:52:02 -0400

    Added premade binary as well as updated chatclient

--- tcpip/chatclient.c
+++ tcpip/chatclient.c
@@ -14,11 +14,48 @@ char inbuf[BUFLEN + 1];
 char outbuf[MAX_MSG_LEN + 1];
 
 int handle_stdin(){
-
+    fflush(stdin);
+    int br = read(STDIN_FILENO, outbuf, MAX_MSG_LEN);
+    outbuf[br - 1] = '\0';
+    if(br-1 > strlen(outbuf)){
+        printf("Sorry, limit your message to %d characters.\n", MAX_MSG_LEN);
+        return 1;
+    }
+    if(strcmp(outbuf, "bye") == 0){
+        printf("Goodbye.\n");
+        return 0;
+    }
+    if(strcmp(outbuf, "") == 0){
+        return 1;
+    }
+    ssize_t bytes_read;
+    if((bytes_read = send(client_socket, outbuf, MAX_MSG_LEN, 0)) == -1){
+        fprintf(stderr, "Error: Failed to send message to server. %s.\n", strerror(errno));
+    }
+    else{
+        return 1;
+    }
 }
 
 int handle_client_socket(){
-
+    int bytes_recvd = recv(client_socket, inbuf, MAX_MSG_LEN, 0);
+    if(bytes_recvd > 0){
+        inbuf[bytes_recvd] = '\0';
+    }
+    else if(bytes_recvd == 0){
+        printf("\nConnection to the server has been lost.\n");
+        return -1;
+    }
+    else{
+        fprintf(stderr, "Failed to receive incoming message. %s.\n", strerror(errno));
+        return -1;
+    }
+    if(strcmp(inbuf, "bye") == 0){
+        printf("Exit\n");
+        return 0;
+    }
+    printf("\n%s\n", inbuf);
+    return 1;
 }
 
 int main(int argc, char* argv[]){
@@ -41,21 +78,21 @@ int main(int argc, char* argv[]){
         fprintf(stderr, "Error: Port must be in range [1024, 65535].\n");
         return EXIT_FAILURE;
     }
-
-    char name[BUFLEN];
-    printf("Hello, what would you like to be called?\n");
+    fflush(stdout);
+    printf("Enter your username: ");
+    fflush(stdout);
     while(true){
         fflush(stdin);
-        int br = read(STDIN_FILENO, name, BUFLEN);
-        name[br - 1] = '\0';
-        if(strlen(name) > MAX_NAME_LEN){
+        int br = read(STDIN_FILENO, username, MAX_NAME_LEN);
+        username[br - 1] = '\0';
+        if(br-1 > MAX_NAME_LEN){
             printf("Sorry, limit your username to %d characters.\n", MAX_NAME_LEN);
         }
-        else if(strlen(name) <= 0){
+        else if(strlen(username) <= 0){
             printf("Username must be at least 1 character.\n");
         }
         else{
-            printf("Hello, %s. Let's try to connect to the server.\n", name);
+            printf("Hello, %s. Let's try to connect to the server.\n", username);
             break;
         }
     }
@@ -71,7 +108,7 @@ int main(int argc, char* argv[]){
         goto EXIT;
     }
     else{
-        int bytes_recvd = recv(client_socket, inbuf, MAX_MSG_LEN-1, 0);
+        int bytes_recvd = recv(client_socket, inbuf, MAX_MSG_LEN, 0);
         if(bytes_recvd > 0){
             inbuf[bytes_recvd] = '\0';
         }
@@ -84,7 +121,41 @@ int main(int argc, char* argv[]){
             goto EXIT;
         }
     }
-    printf("Welcome message: %s\n\n", inbuf);
+    printf("\n%s\n\n", inbuf);
+    //Send username to server
+    ssize_t bytes_read;
+    if((bytes_read = send(client_socket, username, MAX_NAME_LEN, 0)) == -1){
+        fprintf(stderr, "Error: Failed to send username to server. %s.\n", strerror(errno));
+    }
+    
+    while(true){
+        fflush(stdout);
+        printf("[%s]: ", username);
+        fflush(stdout);
+        fd_set fds;
+        FD_ZERO(&fds);
+        FD_SET(client_socket, &fds);
+        FD_SET(STDIN_FILENO, &fds);
+        int retval = select(client_socket+1, &fds, NULL, NULL, NULL);
+        if(retval < 0){
+            fprintf(stderr, "Error: Select failed. %s.\n", strerror(errno));
+        }
+        else{
+            if(FD_ISSET(client_socket, &fds)){
+                int rval = handle_client_socket();
+                if(rval != 1){
+                    goto EXIT;
+                }
+            }
+            else if(FD_ISSET(STDIN_FILENO, &fds)){
+                int rval = handle_stdin();
+                if(rval != 1){
+                    goto EXIT;
+                }
+            }
+        }
+    }
+
 EXIT:
     if(fcntl(client_socket, F_GETFD) >= 0){
         close(client_socket);
